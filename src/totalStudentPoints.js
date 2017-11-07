917d162f1d0800e308d24d7bdc34134269d0a1de
lunchboxer <zixingchepeng@163.com>
2017-11-07 15:07:31 +0800

    clean up totalStudentPoints.js

--- src/totalStudentPoints.js
+++ src/totalStudentPoints.js
@@ -1,13 +1,8 @@
-import { FunctionEvent, fromEvent } from 'graphcool-lib'
-
-// for now we just take the studentId and return all points
-// custom queries can only take a single parameter
+import { fromEvent } from 'graphcool-lib'
 
 export default async event => {
-  console.log("event:", JSON.stringify(event))
 
   // query the api about points filtering by given student
-  // 2 calls : counts of +1 points and counts of -1 points
   const graphcool = fromEvent(event)
   const api = graphcool.api('simple/v1')
 
@@ -34,7 +29,6 @@ export default async event => {
   `
   const variables = { studentId }
   const counts = await api.request(query, variables)
-  console.log("counts:", JSON.stringify(counts))
   const positive = counts.positive.count
   const negative = counts.negative.count
   const total = positive - negative
